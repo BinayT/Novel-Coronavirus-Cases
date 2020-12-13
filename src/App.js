e2ad66c54fcf0b69b263bfa9e7b5d57325e90b3e
Binay <binayprogramming@gmail.com>
2020-12-14 00:48:16 +0100

    Specific country data is now being pulled out from API and shown correctly

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Map from './components/Map';
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
+  const [countryInfo, setCountryInfo] = useState([]);
 
   useEffect(() => {
     async function getData() {
@@ -33,6 +34,19 @@ function App() {
 
   const onCountryChange = async (e) => {
     const selectedCountry = e.target.value;
+    const url =
+      selectedCountry === 'worldwide'
+        ? 'https://disease.sh/v3/covid-19/countries/all'
+        : `https://disease.sh/v3/covid-19/countries/${selectedCountry}`;
+    const { data } = await axios.get(url);
+    setCountryInfo({
+      casesToday: data.todayCases,
+      deathsToday: data.todayDeaths,
+      recoveredToday: data.todayRecovered,
+      totalCases: data.cases,
+      totalDeaths: data.deaths,
+      totalRecovered: data.recovered,
+    });
     setCountry(selectedCountry);
   };
 
@@ -50,7 +64,7 @@ function App() {
             >
               <MenuItem value='worldwide'>Worldwide</MenuItem>
               {countries.map((country) => (
-                <MenuItem value={country.value} key={country.name}>
+                <MenuItem value={country.name} key={country.name}>
                   {country.name}, {country.value}
                 </MenuItem>
               ))}
@@ -59,15 +73,27 @@ function App() {
         </div>
 
         <div className='app__stats'>
-          <InfoBox title='CoronaVirus Cases' cases={2233} total={543221} />
-          <InfoBox title='Recovered' cases={5832} total={4523} />
-          <InfoBox title='Deaths' cases={11645} total={2232} />
+          <InfoBox
+            title='CoronaVirus Cases'
+            cases={countryInfo.casesToday}
+            total={countryInfo.totalCases}
+          />
+          <InfoBox
+            title='Recovered'
+            cases={countryInfo.recoveredToday}
+            total={countryInfo.totalRecovered}
+          />
+          <InfoBox
+            title='Deaths'
+            cases={countryInfo.deathsToday}
+            total={countryInfo.totalDeaths}
+          />
         </div>
 
         <Map />
       </div>
 
-      <Card className='app__left'>
+      <Card className='app__right'>
         <CardContent>
           <h3>Live Cases by Country</h3>
           <h3>Worldwide new cases</h3>
